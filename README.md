be2050d5717401c77a3334f1b2e5c8d3f928c057
Luk_emAn <lucas@empiremedia.de>
2016-11-05 00:39:12 +0100

    Initial Commit
    Ready to go!

---
+++ README.md
@@ -0,0 +1,8 @@
+## SERIES OVERVIEW
+#### (Working Title)
+
+MVP Web App
+
+<br/>
+
+*Liam Krauthoff & Lucas Carl 2016*
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php session_start() ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Series Overview</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	</head>
+	<body>
+
+		
+
+	</body>
+</html>
+
+<script src="./src/js/jquery-3.1.0.min.js"></script>
+<script src="./src/js/main.js"></script>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
