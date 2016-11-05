33d041c7b62bb2f88410f688e00bf9d990e1a5cc
Luk_emAn <lucas@empiremedia.de>
2016-11-05 14:07:04 +0100

    Header Hyperlinked
    Brings you home overtime you click the main title

--- temps/header.inc.php
+++ temps/header.inc.php
@@ -1,5 +1,9 @@
 <div class="container">
-	<h1>Series Overview</h1>
-	<h4>MVP Web App</h4>
+		<h1>
+			<a href="/home" style="color:#333;text-decoration:none">
+				Series Overview
+			</a>
+		</h1>
+		<h4>MVP Web App</h4>
 	<hr/>
 </div>
