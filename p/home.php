5b018b4ef361f887ccbb3f287aca1b09fde1e241
Luk_emAn <lucas@empiremedia.de>
2016-11-05 15:55:59 +0100

    Thumbnail Layout
    New Layout with Thumbnails and hidden Context

--- p/home.php
+++ p/home.php
@@ -19,39 +19,67 @@
 	//+//
 
 
+	$rLimit = 4;
+	$rLength = 0;
+
 	foreach ($set as $data):
+		if ($rLength == 0)
+			echo "<div class='row'>";
 	?>
 
-	<div class="clearfix">
-		<?php if($data->has_img): ?>
-		<div class="col-sm-2">
-			<div style="height:127.5px;width:85.75px;background:url(/assets/img/<?php echo $data->view_id; ?>.jpg) center/cover" title="<?php echo $data->view_title; ?>"></div>
-		</div>
-		<?php endif; ?>
-		<div class="col-sm-4">
-			<h4>
-				<a href="/view/<?php echo $data->view_id ?>">
-					<?php echo $data->view_title ?>
-				</a>
-			</h4>
-			<span>
-				<?php echo $data->view_date; ?>
-				 &bull; <i class="glyphicon glyphicon-time"></i>
-				<?php echo $data->view_dur; ?> mins
-				 &bull; <i class="glyphicon glyphicon-star"></i>
-				<?php echo $data->view_rating; ?>
-			</span>
-		</div>
-		<div class="col-sm-8">
-
-			<p>
-				<?php echo $data->view_desc; ?>
-			</p>
-
+	<div class="thumb-card col-xs-3">
+		<div class="thumb" style="background:url(/assets/img/<?php echo $data->view_id; ?>.jpg) center/cover" title="<?php echo $data->view_title; ?>"></div>
+		<div class="card-context hidden">
+			<div>
+				<h4>
+					<a href="/view/<?php echo $data->view_id ?>">
+						<?php echo $data->view_title ?>
+					</a>
+				</h4>
+				<span>
+					<?php echo $data->view_date; ?>
+					 &bull; <i class="glyphicon glyphicon-time"></i>
+					<?php echo $data->view_dur; ?> mins
+					 &bull; <i class="glyphicon glyphicon-star"></i>
+					<?php echo $data->view_rating; ?>
+				</span>
+			</div>
+			<div>
+				<p>
+					<?php echo $data->view_desc; ?>
+				</p>
+			</div>
 		</div>
 	</div>
-	<hr/>
 
-	<?php endforeach; ?>
+	<?php
+	$rLength++;
+
+	if ($rLength == $rLimit) {
+		$rLength = 0;
+		echo "</div>";
+	}
+	endforeach;
+	?>
 
 </div>
+
+<style>
+	.row {
+		margin-bottom: 30px;
+	}
+	.thumb-card {
+		position: relative;
+		display: inline-block;
+	}
+	.thumb {
+		width: 100%;
+		padding-bottom: 150%;
+	}
+	.card-context {
+		float: left;
+		width: 75vw;
+		left: 0;
+		top: 100%;
+	}
+</style>
