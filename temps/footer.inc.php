9c00ee34204d4afb2f8e12b9d289608ab646171f
lucas-carl <lucas@empiremedia.de>
2016-11-12 11:25:53 +0100

    Added simple footer

---
+++ temps/footer.inc.php
@@ -0,0 +1,7 @@
+<footer class="footer" style="margin: 30px 0">
+	<div class="container">
+		<p class="text-center text-muted">
+			&copy;2016 - Liam Krauthoff & Lucas Carl
+		</p>
+	</div>
+</footer>
