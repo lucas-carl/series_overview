1f2aa44d1278b27322a8e90309c4c39aefb09ed8
lucas-carl <lucas@empiremedia.de>
2016-11-13 13:32:34 +0100

    Added Login/Logout Links to footer

--- temps/footer.inc.php
+++ temps/footer.inc.php
@@ -3,5 +3,13 @@
 		<p class="text-center text-muted">
 			&copy;2016 - Liam Krauthoff & Lucas Carl
 		</p>
+		<ul class="text-center" style="list-style: none;">
+			<li>
+				<a href="/login" class="text-muted">Login</a>
+			</li>
+			<li>
+				<a href="/logout" class="text-muted">Logout</a>
+			</li>
+		</ul>
 	</div>
 </footer>
